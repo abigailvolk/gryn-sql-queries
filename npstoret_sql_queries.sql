07b65abb0cbb5ee5f33d4c43b5dd1f9e2008ca84
Abby Volk <abigail_volk@partner.nps.gov>
2024-02-23 10:29:56 -0700

    added a little more information on local characteristics query

--- npstoret_sql_queries.sql
+++ npstoret_sql_queries.sql
@@ -7,6 +7,7 @@ INNER JOIN tblCharacteristics ON
     tblCharacteristics.LocChDEf_IS_NUMBER=tblResults.LocChDEf_IS_NUMBER;
 
 --Get distinct local characteristic names that are associated with a result (i.e. they have been used for one of the projects)
+-- Purpose: to generate a list of the used Characteristics. May be helpful for joining names to AGOL/lab names.
 SELECT DISTINCT tblCharacteristics.LocCharNameCode
 FROM tblCharacteristics 
 INNER JOIN tblResults ON tblResults.LocCHDEF_IS_NUMBER = tblCharacteristics.LocCHDEF_IS_NUMBER;
